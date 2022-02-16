7f6350fb60998591d2b9cd39a2d32c123231baaf
mimaraslan <mimaraslan@gmail.com>
2022-02-16 04:03:50 +0300

    Apache Kafka, Spring Boot, Cassandra, Spark
    Apache Kafka, Spring Boot, Cassandra, Spark

---
+++ lv1-spring-cassandra-1-crud/src/test/java/com/abcai/SpringBootCassandraCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCassandraCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
