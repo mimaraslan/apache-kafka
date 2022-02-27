05ccd98d7090de55ee38a0a390dae82f19acb1f4
mimaraslan <mimaraslan@gmail.com>
2022-02-27 04:05:01 +0300

    Apache Kafka, Spring Boot, Cassandra, Spark
    Apache Kafka, Spring Boot, Cassandra, Spark

---
+++ lv1-spring-kafka-2-create-post/src/main/java/com/lolo/SpringBootKafkaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.lolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaExampleApplication.class, args);
+	}
+}
