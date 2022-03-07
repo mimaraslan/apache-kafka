4893ea0becffb5684be783ca24641590c38d7dfe
mimaraslan <mimaraslan@gmail.com>
2022-03-07 04:09:15 +0300

    Apache Kafka, Spring Boot, Cassandra, Spark
    Apache Kafka, Spring Boot, Cassandra, Spark

---
+++ lv2-spring-spark-cassandra-1-v2-dontwork/src/main/java/com/lolo/dao/SummaryRowReader.java
@@ -0,0 +1,48 @@
+package com.lolo.dao;
+
+import java.io.Serializable;
+
+import com.datastax.driver.core.Row;
+import com.datastax.spark.connector.CassandraRowMetadata;
+import com.datastax.spark.connector.ColumnRef;
+import com.datastax.spark.connector.cql.TableDef;
+import com.datastax.spark.connector.rdd.reader.RowReader;
+import com.datastax.spark.connector.rdd.reader.RowReaderFactory;
+import com.lolo.model.Summary;
+
+import scala.collection.IndexedSeq;
+
+public class SummaryRowReader extends GenericRowReader<Summary> {
+    private static final long serialVersionUID = 1L;
+    private static RowReader<Summary> reader = new SummaryRowReader();
+
+    public static class SummaryRowReaderFactory implements RowReaderFactory<Summary>, Serializable{
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public RowReader<Summary> rowReader(TableDef arg0, IndexedSeq<ColumnRef> arg1) {
+            return reader;
+        }
+
+        @Override
+        public Class<Summary> targetClass() {
+            return Summary.class;
+        }
+    }
+
+//    @Override
+//    public Summary read(Row row, String[] columnNames) {
+//        Summary summary = new Summary();
+//        summary.setProduct(row.getInt(0));
+//        summary.setSummary(row.getDecimal(1));
+//        return summary;
+//    }
+
+	@Override
+	public Summary read(Row row, CassandraRowMetadata rowMetaData) {
+        Summary summary = new Summary();
+        summary.setProduct(row.getInt(0));
+        summary.setSummary(row.getDecimal(1));
+        return summary;
+	}
+}
---
+++ lv2-spring-spark-cassandra-1-v2-dontwork/src/main/java/com/lolo/dao/SummaryRowWriter.java
@@ -0,0 +1,39 @@
+package com.lolo.dao;
+
+import java.io.Serializable;
+
+import com.datastax.spark.connector.ColumnRef;
+import com.datastax.spark.connector.cql.TableDef;
+import com.datastax.spark.connector.writer.RowWriter;
+import com.datastax.spark.connector.writer.RowWriterFactory;
+import com.lolo.model.Summary;
+
+import scala.collection.IndexedSeq;
+import scala.collection.Seq;
+
+public class SummaryRowWriter implements RowWriter<Summary> {
+    private static final long serialVersionUID = 1L;
+    private static RowWriter<Summary> writer = new SummaryRowWriter();
+
+    // Factory
+    public static class SummaryRowWriterFactory implements RowWriterFactory<Summary>, Serializable{
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public RowWriter<Summary> rowWriter(TableDef arg0, IndexedSeq<ColumnRef> arg1) {
+            return writer;
+        }
+    }
+
+    @Override
+    public Seq<String> columnNames() {
+        return scala.collection.JavaConversions.asScalaBuffer(Summary.columns()).toList();
+    }
+    
+    @Override
+    public void readColumnValues(Summary summary, Object[] buffer) {
+        buffer[0] = summary.getProduct();
+        buffer[1] = summary.getSummary();
+    }
+    
+}
