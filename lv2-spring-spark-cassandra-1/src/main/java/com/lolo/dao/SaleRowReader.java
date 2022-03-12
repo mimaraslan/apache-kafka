8e2f8c63b8f125ba557add32e5aa83facc2d3e82
mimaraslan <mimaraslan@gmail.com>
2022-03-12 04:10:19 +0300

    Apache Kafka, Spring Boot, Cassandra, Spark
    Apache Kafka, Spring Boot, Cassandra, Spark

---
+++ lv2-spring-spark-cassandra-1/src/main/java/com/lolo/dao/SaleRowReader.java
@@ -0,0 +1,49 @@
+package com.lolo.dao;
+
+import java.io.Serializable;
+
+import com.datastax.driver.core.Row;
+import com.datastax.spark.connector.CassandraRowMetadata;
+import com.datastax.spark.connector.ColumnRef;
+import com.datastax.spark.connector.cql.TableDef;
+import com.datastax.spark.connector.rdd.reader.RowReader;
+import com.datastax.spark.connector.rdd.reader.RowReaderFactory;
+import com.lolo.model.Sale;
+
+import scala.collection.IndexedSeq;
+
+public class SaleRowReader extends GenericRowReader<Sale> {
+    private static final long serialVersionUID = 1L;
+    private static RowReader<Sale> reader = new SaleRowReader();
+
+    public static class SaleRowReaderFactory implements RowReaderFactory<Sale>, Serializable{
+        private static final long serialVersionUID = 1L;
+        @Override
+        public RowReader<Sale> rowReader(TableDef arg0, IndexedSeq<ColumnRef> arg1) {
+            return reader;
+        }        
+
+        @Override
+        public Class<Sale> targetClass() {
+            return Sale.class;
+        }
+    }
+
+//    @Override
+//    public Sale read(Row row, String[] columnNames) {
+//        Sale sale = new Sale();
+//        sale.setId(row.getUUID(0));
+//        sale.setPrice(row.getDecimal(1));
+//        sale.setProduct(row.getInt(2));
+//        return sale;
+//    }
+
+	@Override
+	public Sale read(Row row, CassandraRowMetadata rowMetaData) {
+        Sale sale = new Sale();
+        sale.setId(row.getUUID(0));
+        sale.setPrice(row.getDecimal(1));
+        sale.setProduct(row.getInt(2));
+        return sale;
+	}
+}
---
+++ lv2-spring-spark-cassandra-1/src/main/java/com/lolo/dao/SaleRowWriter.java
@@ -0,0 +1,40 @@
+package com.lolo.dao;
+
+import java.io.Serializable;
+
+import com.datastax.spark.connector.ColumnRef;
+import com.datastax.spark.connector.cql.TableDef;
+import com.datastax.spark.connector.writer.RowWriter;
+import com.datastax.spark.connector.writer.RowWriterFactory;
+import com.lolo.model.Sale;
+
+import scala.collection.IndexedSeq;
+import scala.collection.Seq;
+
+public class SaleRowWriter implements RowWriter<Sale> {
+    private static final long serialVersionUID = 1L;
+    private static RowWriter<Sale> writer = new SaleRowWriter();
+
+    // Factory
+    public static class SaleRowWriterFactory implements RowWriterFactory<Sale>, Serializable{
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public RowWriter<Sale> rowWriter(TableDef arg0, IndexedSeq<ColumnRef> arg1) {
+            return writer;
+        }
+    }
+
+    @Override
+    public Seq<String> columnNames() {
+        return scala.collection.JavaConversions.asScalaBuffer(Sale.columns()).toList();
+    }
+    
+    @Override
+    public void readColumnValues(Sale sale, Object[] buffer) {
+        buffer[0] = sale.getId();
+        buffer[1] = sale.getProduct();
+        buffer[2] = sale.getPrice();     
+    }
+    
+}
