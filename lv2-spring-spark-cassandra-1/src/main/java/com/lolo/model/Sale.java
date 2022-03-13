d56816a74f96325d23a998fa39f1786dad75820f
mimaraslan <mimaraslan@gmail.com>
2022-03-13 04:10:34 +0300

    Apache Kafka, Spring Boot, Cassandra, Spark
    Apache Kafka, Spring Boot, Cassandra, Spark

---
+++ lv2-spring-spark-cassandra-1/src/main/java/com/lolo/model/Sale.java
@@ -0,0 +1,61 @@
+package com.lolo.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Sale implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private UUID id;
+    private Integer product;
+    private BigDecimal price;
+
+    public Sale() {
+    }
+
+    public Sale(UUID id, Integer product, BigDecimal price) {
+        this.id = id;
+        this.product = product;
+        this.price = price;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Integer getProduct() {
+        return product;
+    }
+
+    public void setProduct(Integer product) {
+        this.product = product;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Sale'{'id={0}, product={1}, price={2}'}'", id, product, price);
+    }
+    
+    public static List<String> columns() {
+        List<String> columns = new ArrayList<String>();
+        columns.add("id");
+        columns.add("product");
+        columns.add("price");
+        return columns;
+    }
+}
---
+++ lv2-spring-spark-cassandra-1/src/main/java/com/lolo/model/Summary.java
@@ -0,0 +1,49 @@
+package com.lolo.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Summary implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer product;
+    private BigDecimal summary;
+
+    public Summary() {
+    }
+
+    public Summary(Integer product, BigDecimal summary) {
+        this.product = product;
+        this.summary = summary;
+    }
+
+    public Integer getProduct() {
+        return product;
+    }
+
+    public void setProduct(Integer product) {
+        this.product = product;
+    }
+
+    public BigDecimal getSummary() {
+        return summary;
+    }
+
+    public void setSummary(BigDecimal summary) {
+        this.summary = summary;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Summary'{'product={0}, summary={1}'}'", product, summary);
+    }
+    
+    public static List<String> columns() {
+        List<String> columns = new ArrayList<String>();
+        columns.add("product");
+        columns.add("summary");
+        return columns;
+    }
+}
