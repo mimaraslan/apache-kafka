27c5aa67675bd0b7731527e0b1d75d2f6187a6ff
mimaraslan <mimaraslan@gmail.com>
2022-03-31 04:16:50 +0300

    Apache Kafka, Spring Boot, Cassandra, Spark
    Apache Kafka, Spring Boot, Cassandra, Spark

---
+++ lv3-spring-kafka-spark-cassandra-dontwork/src/test/java/com/loloia/SpringBootCassandraCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loloia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCassandraCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
